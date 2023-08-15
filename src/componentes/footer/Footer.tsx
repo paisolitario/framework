dd2bbae2ae756ce534b5fa518de541caeb12a372
TABATA WENDY MENEZES MARQUES <07609109100@ifms.edu.br>
2023-08-15 07:34:10 -0400

    first commit

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+function Footer(){
+    return(
+        <footer>
+            <h1> MAMAMIA MARXELO!</h1>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,28 @@
+import './Header.css'
+export default function Header() {
+  return (
+    <header>
+      <img src='./src/componentes/header/barbie.png'  className='barbie-img'/>
+      <div>
+       
+        <h2 className='oi'>𝓼𝓮𝓳𝓪 𝓺𝓾𝓮𝓶 𝓿𝓸𝓬𝓮 𝓺𝓾𝓲𝓼𝓮𝓻!</h2>
+      </div>
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href='#'>SOBRE</a>
+          </li>
+          <li>
+            <a href='#'>PRODUTOS</a>
+          </li>
+          <li>
+            <a href='#'>NOVIDADES</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
